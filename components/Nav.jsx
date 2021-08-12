032a075c83dbb52cafb153290287e8f6d2ea02c6
ossamaelidrissi <87373195+ossamaelidrissi@users.noreply.github.com>
2021-08-12 17:26:25 +0200

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ function Nav() {
     const router = useRouter();
     return (
         <nav className = 'relative'>
-            <div className = "flex justify-evenly px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
+            <div className = "flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
             {Object.entries(requests).map(([key, { title,url}]) => (
                 <h2 
                     key = {key} 
